8e89d3c643a13df42edc512fcde6917bcb8f2d66
alexismathieu-ui <alexis.mathieupro45170@gmail.com>
2026-01-13 13:27:48 +0100

    TP5

--- TP5/Player.js
+++ TP5/Player.js
@@ -12,14 +12,29 @@ class Player {
     this.kill = false;
     this.killCount = 0;
     this.cooldown = 2;
-    this.Isalive = true;
+    this.isalive = true;
     this.regenActive = false;
     this.regen = 5;
-    this.MaxHealth = 100;
-    this.MaxCooldown = 2;
-    this.HealthBar = 100;
-    this.direction = right;
-    this.Isattack = false;
+    this.maxhealth = 100;
+    this.maxcooldown = 2;
+    this.healthbar = 100;
+    this.direction = 0;
+    this.isattack = false;
+    this.iswalking = true;
+    this.isdying = false;
+    this.walkspriteduration = 2;
+    this.walkspriteindex = 0;
+    this.walkspritenumber = 9;
+    this.attackspriteindex = 0;
+    this.attackspriteduration = 2;
+    this.attackspritenumber = 8;
+    this.dyingspriteduration = 2;
+    this.dyingspriteindex = 0;
+    this.dyingspritenumber = 6;
+    this.idle = 0;
+    this.currentwalking = 0;
+    this.currentattacking = 0;
+    this.currentdying = 0;
   }
   update(updateData) {
     this.health = updateData.health;
@@ -31,10 +46,112 @@ class Player {
     this.positionX = updateData.positionX;
     this.positionY = updateData.positionY;
     this.kill = false;
-    this.MaxHealth = updateData.MaxHealth;
-    this.MaxCooldown = updateData.MaxCooldown;
-    this.Isalive = false;
+    this.maxhealth = updateData.maxhealth;
+    this.maxcooldown = updateData.maxcooldown;
+    this.isalive = false;
     this.direction = updateData.direction;
-    this.Isattack = updateData.attack;
+    this.isattack = updateData.isattack;
+    this.iswalking = updateData.iswalking;
+    this.isdying = updateData.isdying;
+    this.isalive = updateData.isalive;
+    this.walkspriteduration = updateData.walkspriteduration;
+    this.walkspriteindex = updateData.walkspriteindex;
+    this.walkspritenumber = updateData.walkspritenumber;
+    this.attackspriteindex = updateData.attackspriteindex;
+    this.attackspriteduration = updateData.attackspriteduration;
+    this.attackspritenumber = updateData.attackspritenumber;
+    this.dyingspriteduration = updateData.dyingspriteduration;
+    this.dyingspriteindex = updateData.dyingspriteindex;
+    this.dyingspritenumber = updateData.dyingspritenumber;
+    this.idle = updateData.idle;
+    this.currentwalking = updateData.currentwalking;
+    this.currentattacking = updateData.currentattacking;
+    this.currentdying = updateData.currentdying;
   }
+
+  Animate() {
+    //THe player is walking
+    if (this.iswalking) {
+      this.currentwalking++;
+      if (this.currentwalking >= this.walkspriteduration) {
+        this.currentwalking = 0;
+        this.walkspriteindex++;
+      }
+      if (this.walkspriteindex >= this.walkspritenumber) {
+        this.walkspriteindex = 0;
+      }
+    }
+
+    //The player is attacking
+    else if (this.isattack) {
+      this.currentattacking++;
+      if (this.currentattacking >= this.attackspriteduration) {
+        this.currentattacking = 0;
+        this.attackspriteindex++;
+      }
+      if (this.attackspriteindex >= this.attackspritenumber) {
+        this.attackspriteindex = 0;
+      }
+    }
+    //The player is dying
+    else if (this.isdying) {
+      this.currentdying++;
+      if (this.currentdying >= this.dyingspriteduration) {
+        this.currentdying = 0;
+        this.dyingspriteindex++;
+      }
+      if (this.dyingspriteindex >= this.dyingspritenumber) {
+        this.dyingspriteindex = this.dyingspritenumber - 1;
+      }
+    }
+    //The player is idle
+    else {
+      this.idle++;
+      if (this.idle >= 10) {
+        this.idle = 0;
+      }
+    }
+    if (!this.iswalking) {
+      this.currentwalking = 0;
+      this.walkspriteindex = 0;
+    }
+    if (!this.isattack) {
+      this.currentattacking = 0;
+      this.attackspriteindex = 0;
+    }
+    if (!this.isdying) {
+      this.currentdying = 0;
+      this.dyingspriteindex = 0;
+    }
+
+    console.log("Walk animation :\n");
+    console.log("iswalking = ", this.iswalking);
+    console.log(
+      "walkspriteindex = ",
+      this.walkspriteindex,
+      "/",
+      this.walkspritenumber
+    );
+    // console.log("Attack animation :\n");
+    // console.log("isattack = ", this.isattack);
+    // console.log(
+    //   "attackspriteindex = ",
+    //   this.attackspriteindex,
+    //   "/",
+    //   this.attackspritenumber
+    // );
+    // console.log("Dying animation :\n");
+    // console.log("isdying = ", this.isdying);
+    // console.log(
+    //   "dyingspriteindex = ",
+    //   this.dyingspriteindex,
+    //   "/",
+    //   this.dyingspritenumber
+    // );
+  }
+}
+
+toto = new Player(42, "toto", 1, [0, 0]);
+for (let i = 0; i < 13; i++) {
+  toto.Animate();
 }

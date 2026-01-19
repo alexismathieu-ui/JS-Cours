a099fceb4218b66ef8026b223de011d2eb8c365a
alexismathieu-ui <alexis.mathieupro45170@gmail.com>
2026-01-19 10:15:39 +0100

    TP 7

--- TP7/Game.js
+++ TP7/Game.js
@@ -53,9 +53,9 @@ class Game {
     this.isOver = gameStateFromServer.isOver;
     this.timer = gameStateFromServer.timer;
     //Update players
-    const serverPlayerIds = new Set(Object.keys(gameStateFromServer.players));
+    const serverPlayerId = new Set(Object.keys(gameStateFromServer.players));
     // Add or update players from server data
-    for (const playerId of serverPlayerIds) {
+    for (const playerId of serverPlayerId) {
       const playerData = gameStateFromServer.players[playerId];
       if (this.players[playerId]) {
         // Player exists, update it
@@ -73,7 +73,7 @@ class Game {
 
     //Remove players not present in server data
     for (const playerId of Object.keys(this.players)) {
-      if (!serverPlayerIds.has(playerId)) {
+      if (!serverPlayerId.has(playerId)) {
         delete this.players[playerId];
       }
     }
@@ -86,7 +86,7 @@ console.log("Initial game state:", game);
 game.update(backendData);
 
 // Simulate a player leaving and a new player joining
-const modifiedBackendData = JSON.parse(JSON.stringify(backendData));
+const modifiedBackendData = backendData;
 console.log(
   "Le Joueur",
   modifiedBackendData.players["3cd71bbb-6a6b-4d4e-80e3-107130328a27"].name,
@@ -94,7 +94,7 @@ console.log(
 );
 delete modifiedBackendData.players["3cd71bbb-6a6b-4d4e-80e3-107130328a27"];
 modifiedBackendData.players["new-player-id"] = {
-  name: "Fabrice",
+  name: "Titouan",
   skinPath: "./assets/8.png",
   position: [0.788555452214, 0.185663324],
   lvl: 1,
@@ -109,9 +109,34 @@ modifiedBackendData.players["new-player-id"] = {
   attackCooldown: 1,
   currentAttackCooldown: 0,
 };
+game.update(modifiedBackendData);
 console.log(
   "Le Joueur",
   modifiedBackendData.players["new-player-id"].name,
   "A rejoint la partie"
 );
+modifiedBackendData.players["garlic"] = {
+  name: "Cristalline",
+  skinPath: "./assets/21.png",
+  position: [0.426812357, 0.96486521545],
+  lvl: 1,
+  hp: 100,
+  maxHp: 100,
+  hpRegenRate: 10,
+  speed: 0.2,
+  direction: 1,
+  isAttacking: false,
+  isWalking: false,
+  isDying: false,
+  attackCooldown: 1,
+  currentAttackCooldown: 0,
+};
+game.update(modifiedBackendData);
+console.log(
+  "Le Joueur",
+  modifiedBackendData.players["garlic"].name,
+  "A rejoint la partie"
+);
+
+modifiedBackendData.players["28ead291-fcea-4b41-a596-d3c876c49a53"].hp = 80;
 game.update(modifiedBackendData);
